ca434f8bc3ceaf34ecbf143926278d6c9dddee9f
Nick Gilbert <nick.gilbert@worldvision.org.uk>
2020-05-18 17:25:10 +0100

    Removed ViewBag references and added MyViewData class instead

--- Dynamics 365 WebApp/Controllers/HomeController.cs
+++ Dynamics 365 WebApp/Controllers/HomeController.cs
@@ -13,47 +13,42 @@ namespace Dynamics_365_WebApp.Controllers
             var (crmConnection, service) = new DynamicsConnection().ConnectToDynamics();
             var (paginatedContactList, nextPageNumber, hasPreviousPage, hasNextPage) = new NewGetContactList().GetListOfContacts(crmConnection, search, option, currentPageNumber);
 
-            ViewBag.CrmConnection = (service != null) ? true : false;
-            ViewBag.RadioButtonSelected = (option == null) ? "Last Name" : option;
-            ViewBag.SearchValue = search;
-            ViewBag.PageNumber = nextPageNumber;
-            ViewBag.HasPreviousPage = hasPreviousPage;
-            ViewBag.HasNextPage = hasNextPage;
-
+            MyViewData.SetData(service, option, search, nextPageNumber, hasPreviousPage, hasNextPage, null);
+            
             return View(paginatedContactList);
         }
 
         public ActionResult Create(string option, string search, int? currentPageNumber)
         {
-            ViewBag.CrmConnection = true;
-            ViewBag.RadioButtonSelected = (option == null) ? "Last Name" : option;
-            ViewBag.SearchValue = search;
-            ViewBag.PageNumber = currentPageNumber;
+            var (_, service) = new DynamicsConnection().ConnectToDynamics();
+            MyViewData.SetData(service, option, search, currentPageNumber, null, null, null);
+            
             return View();
         }
 
         [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Create(Contact newContact, string searchValue, string selectionValue, int? currentPageNumber)
+        public ActionResult Create(Contact newContact, string search, string option, int? currentPageNumber)
         {
            
             var (_, service) = new DynamicsConnection().ConnectToDynamics();
             var success = new CreateContact().AddContactToDynamics(newContact, service);
 
-            ViewBag.CrmConnection = (service != null) ? true : false;
+            MyViewData.SetData(service, option, search, currentPageNumber, null, null, null);
 
             if (success)
             {
                 var pageNumber = (currentPageNumber > 0) ? currentPageNumber - 1 : null;
-                return RedirectToAction("Index", new { option = selectionValue, search = searchValue, currentPageNumber = pageNumber });
+                return RedirectToAction("Index", new { option = option, search = search, currentPageNumber = pageNumber });
             }
             else
             {
-                ViewBag.ErrorMessage = "New record creation failed.";
+                MyViewData.SetData(service, option, search, currentPageNumber, null, null, "New record creation failed.");
+                
                 return View();
             }
         }
 
-         public ActionResult DeleteContact(string id, string searchValue, string selectionValue, int? currentPageNumber)
+         public ActionResult DeleteContact(string id, string search, string option, int? currentPageNumber)
         {
             var (_, service) = new DynamicsConnection().ConnectToDynamics();
             Entity contact = new Entity("contact");
@@ -63,41 +58,36 @@ namespace Dynamics_365_WebApp.Controllers
                 service.Delete(contact.LogicalName, Guid.Parse(id));
             }
             var pageNumber = (currentPageNumber > 0) ? currentPageNumber - 1 : null;
-            return RedirectToAction("Index", new { option = selectionValue, search = searchValue, currentPageNumber = pageNumber });
+            return RedirectToAction("Index", new { option = option, search = search, currentPageNumber = pageNumber });
         }
 
-        public ActionResult EditContact(string id, string searchValue, string selectionValue, int? currentPageNumber)
+        public ActionResult EditContact(string id, string search, string option, int? currentPageNumber)
         {
             var (crmConnection, service) = new DynamicsConnection().ConnectToDynamics();
             var updateContact = new NewGetContact().GetContact(crmConnection, id);
 
-            ViewBag.CrmConnection = (service != null) ? true : false;
-            ViewBag.RadioButtonSelected = selectionValue;
-            ViewBag.SearchValue = searchValue;
-            ViewBag.PageNumber = currentPageNumber;
+            MyViewData.SetData(service, option, search, currentPageNumber, null, null, null);
+
             return View(updateContact);
         }
 
         [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult EditContact(Contact updatedContact, string searchValue, string selectionValue, int? currentPageNumber)
+        public ActionResult EditContact(Contact updatedContact, string search, string option, int? currentPageNumber)
         {
             var (_, service) = new DynamicsConnection().ConnectToDynamics();
             var success = new UpdateContact().UpdateContactData(service, updatedContact);
-
-            ViewBag.CrmConnection = (service != null) ? true : false;
-
+            
+            MyViewData.SetData(service, option, search, currentPageNumber, null, null, null);
+            
             if (success)
             {
                 var pageNumber = (currentPageNumber > 0) ? currentPageNumber - 1 : null;
-                return RedirectToAction("Index", new {option = selectionValue, search = searchValue, currentPageNumber = pageNumber });
+                return RedirectToAction("Index", new {option = option, search = search, currentPageNumber = pageNumber });
             }
             else
             {
-                ViewBag.ErrorMessage = "Record update failed.";
-                ViewBag.CrmConnection = (service != null) ? true : false;
-                ViewBag.RadioButtonSelected = selectionValue;
-                ViewBag.SearchValue = searchValue;
-                ViewBag.PageNumber = currentPageNumber;
+                MyViewData.SetData(service, option, search, currentPageNumber, null, null, "Record update failed.");
+
                 return View();
             }
         }
---
+++ Dynamics 365 WebApp/Models/MyViewData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dynamics_365_WebApp.Models
+{
+    public static class MyViewData
+    {
+        public static bool CrmConnection;
+        public static string RadioButtonSelected;
+        public static string Search;
+        public static int? PageNumber;
+        public static bool? HasPreviousPage;
+        public static bool? HasNextPage;
+        public static string Message;
+           
+        public static void SetData(IOrganizationService service, string option, string search, int? pageNumber, bool? hasPreviousPage, bool? hasNextPage, string message)
+        {
+            CrmConnection = (service != null) ? true : false;
+            RadioButtonSelected = (option == null) ? "Last Name" : option;
+            Search = search;
+            PageNumber = pageNumber;
+            if (hasPreviousPage != null) HasPreviousPage = hasPreviousPage;
+            if (hasNextPage != null) HasNextPage = hasNextPage;
+            if (message != null) Message = message;
+        }
+    }
+}
